fdb14d7696c2a9148a78185047003f344d43c159
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-02-03 10:26:10 +0100

    Username kleiner dan 4 tekens = foutmelding

--- Test Git/Action_page.php
+++ Test Git/Action_page.php
@@ -1,17 +1,24 @@
 <html>
+    <link rel="stylesheet" href="style.css">
 <?php 
+echo "<br>";
+
+
 date_default_timezone_set("Europe/Amsterdam");
 echo "De datum van vandaag is " . date ("D-m-y") . "<br>"; 
 /* print username */
 echo "Username:" . $_POST ["Uname"] . "<br>";
+echo "Password:" . $_POST ["Pname"] . "<br>";
 //var_dump($_POST);
 
 
 /* print een foutmelding als username < 5 tekens */
+$len = strlen($_POST['Uname']);
+echo "de lengte van uname: $len";
 
-echo "Password:" . $_POST ["Pname"] . "<br>";
-//var_dump($_POST);
-/* print een foutmelding als password <5 tekens */
-
+if ( $len < 5) {
+    echo "<br>Username moet groter zijn dan 4 tekens <br>";
+}
+//var_dump($_POST);//
 ?>
 </html>
