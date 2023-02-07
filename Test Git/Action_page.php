7d47e2d097b0da51c8ffca074cdf7323c098419e
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-02-07 13:12:46 +0100

    Arrays gemaakt

--- Test Git/Action_page.php
+++ Test Git/Action_page.php
@@ -3,6 +3,17 @@
 <?php 
 echo "<br>";
 
+$a[0] = "Jan";
+$a[1] = "Rob";
+$a[2] = "Piet";
+
+foreach($a as $value){
+    //print naam
+    echo $value . "<br>";
+}
+
+//var_dump($a);
+echo "<br>";
 
 date_default_timezone_set("Europe/Amsterdam");
 echo "De datum van vandaag is " . date ("D-m-y") . "<br>"; 
@@ -14,14 +25,15 @@ echo "Password:" . $_POST ["Pname"] . "<br>";
 
 /* print een foutmelding als username < 5 tekens */
 $len = strlen($_POST['Uname']);
-echo "de lengte van uname: $len";
+echo "De lengte van de username: $len";
 
 if ( $len < 5) {
     echo "<br>Username moet groter zijn dan 4 tekens <br>";
 }
+/* print een foutmelding als username > 10 tekens */
 else if ($len > 10) {
     echo "<br>Username moet kleiner zijn dan 10 tekens <br>";
 }
-//var_dump($_POST);//
+//var_dump($_POST);
 ?>
 </html>
