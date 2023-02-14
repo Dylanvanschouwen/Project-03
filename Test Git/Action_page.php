62cfee80e6c9050f68503f31e6ef5764ff6a1d0b
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-02-14 13:16:01 +0100

    Functions.php en main-bieren.php ready

--- Test Git/Action_page.php
+++ Test Git/Action_page.php
@@ -1,6 +1,8 @@
 <html>
     <link rel="stylesheet" href="style.css">
 <?php 
+
+/*var_dump ($_POST);*/
 echo "<br>";
 
 $a[0] = "Jan";
@@ -9,13 +11,12 @@ $a[2] = "Piet";
 
 $b = array(10,11,2,34,100,1000);
 
-echo '<table border= "1" width= "200">';
-foreach($b as $value){
+echo '<table border= "1" width= "100">';
+foreach($_POST as $value){
     //toevoegen tr en td
     echo "<tr><td>";
     //print naam
     echo $value . "<br>";
-
     //afsluiten tr en td
     echo "</td></tr>";
 }
@@ -42,16 +43,6 @@ if ( $len < 5) {
 /* print een foutmelding als username > 10 tekens */
 else if ($len > 10) {
     echo "<br>Username moet kleiner zijn dan 10 tekens <br>";
-}
-echo '<table border= "1" width= "200">';
-foreach($_POST as $value){
-    //toevoegen tr en td
-    echo "<tr><th><td>";
-    //print naam
-    echo $value . "<br>";
-
-    //afsluiten tr en td
-    echo "</td></th></tr>";
 }
 //var_dump($_POST);
 ?>
