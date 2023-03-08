3a321934a899e0cd7b072b3c89ca8cf6279ddc74
Jack <9019233@student.zadkine.nl>
2023-03-08 12:01:32 +0100

    small nav change + klachten tussen page afgemaakt

--- Webpages/klachten.php
+++ Webpages/klachten.php
@@ -1,4 +1,4 @@
-<!-- This is the homepage -->
+<!-- This is the complaints/compliments between page -->
 <!-- Author: Jack -->
 
 <!-- Include head.html -->
@@ -12,8 +12,19 @@ include "../Webpages/include pages/navbar.php";
 
 
 <!-- Main code -->
-<main>
-    
+<main id="klachtpage_main">
+    <header id="klachtpage_header">
+        <h1>Heeft u een klacht of compliment? Dien deze in met de formulieren hieronder!</h1>
+    </header>
+
+    <section>
+        <ul id="klachtpage_list">
+            <li><a href="#">Klacht over de website</a></li>
+            <li><a href="#">Klacht over een product</a></li>
+            <li><a href="./mederwerkerklachten.php">Klacht over een medewerker</a></li>
+            <li><a href="#">Compliment geven</a></li>
+        </ul>
+    </section>
 </main>
 
 
