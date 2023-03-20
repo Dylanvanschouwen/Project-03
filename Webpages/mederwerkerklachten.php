7f302bcd1bb23414fbdcb2bbe52e796b0e08efbd
Zrynix <9018744@student.zadkine.nl>
2023-03-20 14:06:28 +0100

    aanpassingen

--- Webpages/mederwerkerklachten.php
+++ Webpages/mederwerkerklachten.php
@@ -1,9 +1,13 @@
 <form method="post" action="">
     Naam: <input type="text" name="naam" id="naam"></input></br></br>
-    Bericht <textarea type="text" name="bericht" id="bericht"></textarea></br></br>
+    Bericht: <textarea type="text" name="bericht" id="bericht"></textarea></br></br>
+    Medewerker: <textarea type="text" name="medewerker" id="Medewerker"></textarea></br></br>
+    Straatnaam: <textarea type="text" name="straatnaam" id="Straatnaam"></textarea></br></br>
+    Huisnummer: <textarea type="text" name="huisnummer" id="huisnummer"></textarea></br></br>
+    Contact: <textarea type="text" name="contact" id="contact"></textarea></br></br>
+     <textarea type="text" name="bericht" id="bericht"></textarea></br></br>
     <input type="submit" name="knop" id="knop">
 </form>
-
 
 <?php
 
@@ -16,39 +20,20 @@ $password = "";
 $dbname = "klachtenform";
 
 // connectiem maken met de PDO.
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password) ;
-$conn->setAttribute(PDO: ATTR_ERRMODE ,  PDO: ERRMODE_EXCEPTION) ;
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+if (isset($_POST["knop"])){
+  echo ("verzend knop werkt");
+  $naam = $_POST ["naam"];
+  $bericht = $_POST ["bericht"];
+  $queryupdate = $conn->prepare("INSERT INTO Klachten(berichten, naam) VALUES ('$bericht', '$naam')");
+  $queryupdate->execute();
 
-?>
-
-
-try {
-    // INSERT query uitvoeren
-    $stmt = $conn->prepare ("INSERT INTO berichten (naam, bericht, datumtijd)
-    VALUES (:naam , :bericht, :datumtijd)");
-    $stmt->bindParam (':naam', $naam);
-    $stmt->bindParam (':bericht', $bericht);
-    $stmt->bindParam (':datumtijd', $datumtijd);
-
-    // insert rij
-if (isset($_RESQUEST['naam']))
-{
-    $naam = $_POST['naam'];
-    $bericht = $_POST['bericht'];
-    $datumtijd = Date('d-m-y');
-    $stmt->execute();
-
-    // terugsturen naar de hoofdpagina
-    header('Location: index.php');
-}}
-catch(PDOException $e)
-{
-    echo "Error: " . $e->getMessage();
 }
-$sqlSelect = "SELECT * from berichten";
-$data = $conn->query($sqlSelect);
 
-$conn = null;
+
+
+
+
 ?>
 
 
@@ -65,4 +50,4 @@ $conn = null;
 
 
 
-}
+
