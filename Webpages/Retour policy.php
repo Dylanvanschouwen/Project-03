00cf2a13dbd37bf29fef530e09cc39970f6fb4c0
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-03-22 10:31:28 +0100

    Merge branch 'main' of https://github.com/Dylanvanschouwen/Project-03

--- Webpages/Retour policy.php
+++ Webpages/Retour policy.php
@@ -12,16 +12,16 @@
 
 <!-- Include Retour policy.js -->
     <script 
-    type="text/javascript" src="test.js">
+    type="text/javascript" src="RP.js">
     </script>
 
 <body>
 <!-- Main code -->
 <main id="RP_main">
-    <header id="RP_header">  </header>
-        <h1 id="RPheader_1">Retour policy uitleg.</h1>
-        <h2 id="RPheader_2"> awdbhkadbwkahbdwkahb</h2>
-
+    <header class="RP_header">  
+        <h1 class="RPheader_1">Retour policy uitleg.</h1>
+        <h5 class="RPheader_2">Een product retourneer je binnen 30 dagen na ontvangst, compleet, in nieuwstaat en, indien redelijkerwijs mogelijk, in de originele verpakking. Je mag het product gerust bekijken en proberen zoals je dat ook in onze winkel kunt doen. <br> We verzoeken je om het product zodanig te retourneren dat wij het product via onze MediaMarkt Outlet een tweede leven kunnen geven. Zo dragen wij samen bij aan een duurzaam gebruik van consumentenelektronica. </h5>
+    </header>
     
     <div class="RP_section1">
     <div class="RPkop_1">Div 1</div>
@@ -30,10 +30,10 @@
     </div>    
 
     <br>
-
+    
     <div class="RP-row_1">
     <div class="RPdropdown_1" onclick="toggleRPdropdown_1()">
-    <button>Click me</button>
+    <div class="RP_dropdown" role="button"></div> 
     <div class="RPdropdown_1-content" id="RPdropdown_ID_1">
     <h1>text</h1>
     </div>
@@ -53,10 +53,82 @@
     </div>
     </div>
   </div>
+
+  <div class="RP-row_2">
+  <div class="RPdropdown_4" onclick="toggleRPdropdown_4()">
+    <button>Click me</button>
+    <div class="RPdropdown_4-content" id="RPdropdown_ID_4">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_5" onclick="toggleRPdropdown_5()">
+    <button>Click me</button>
+    <div class="RPdropdown_5-content" id="RPdropdown_ID_5">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_6" onclick="toggleRPdropdown_6()">
+    <button>Click me</button>
+    <div class="RPdropdown_6-content" id="RPdropdown_ID_6">
+    <h1>text</h1>
+    </div>
+    </div>
+  </div>
+
+  <div class="RP-row_3">
+  <div class="RPdropdown_7" onclick="toggleRPdropdown_7()">
+    <button>Click me</button>
+    <div class="RPdropdown_7-content" id="RPdropdown_ID_7">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_8" onclick="toggleRPdropdown_8()">
+    <button>Click me</button>
+    <div class="RPdropdown_8-content" id="RPdropdown_ID_8">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_9" onclick="toggleRPdropdown_9()">
+    <button>Click me</button>
+    <div class="RPdropdown_9-content" id="RPdropdown_ID_9">
+    <h1>text</h1>
+    </div>
+    </div>
+</div>
+
+<div class="RP-row_4">
+  <div class="RPdropdown_10" onclick="toggleRPdropdown_10()">
+    <button>Click me</button>
+    <div class="RPdropdown_10-content" id="RPdropdown_ID_10">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_11" onclick="toggleRPdropdown_11()">
+    <button>Click me</button>
+    <div class="RPdropdown_11-content" id="RPdropdown_ID_11">
+    <h1>text</h1>
+    </div>
+  </div>
+
+  <div class="RPdropdown_12" onclick="toggleRPdropdown_12()">
+    <button>Click me</button>
+    <div class="RPdropdown_12-content" id="RPdropdown_ID_12">
+    <h1>text</h1>
+    </div>
+    </div>
+ </div>
+  <!-- Include footer.html -->
+
+  <?php
+include "../Webpages/include pages/footer.php";
+?>
+
 </main>
-
-
-
 </body>
 
 
