5198773c8929c7f5b758379d60f27b0d79006683
Jack <9019233@student.zadkine.nl>
2023-04-11 16:51:49 +0200

    shitty categorieen toevoegen, veranderen naar crud

--- Webpages/include pages/navbar.php
+++ Webpages/include pages/navbar.php
@@ -22,15 +22,8 @@
         <ul class="list_onder">
             <li class="categorie_dropdown_c">
                 <div class="categorie_btn_c">
-                    <svg id="dropdown_bars1" viewBox="0 0 80 80"><path d="M4.726 9.507c-2.66 0-4.56 2.279-4.56 4.938s2.28 4.56 4.94 4.56h70.283c2.66 0 4.56-1.9 4.56-4.56s-1.9-4.938-4.56-4.938H4.726zm0 25.834c-2.66 0-4.56 1.9-4.56 4.559s2.28 4.559 4.94 4.559h70.283c2.66 0 4.56-1.9 4.56-4.56s-1.9-4.558-4.56-4.558H4.726zm0 25.454c-2.66 0-4.94 2.28-4.94 4.939s2.28 4.559 4.94 4.559H75.01c2.659 0 4.938-2.28 4.938-4.94s-2.28-4.938-4.938-4.938H4.726v.38z"></path></svg>
-                    <a href="">Categorieën bekijken</a>  
+                    <a href="categorieenform.php">Categorieën bekijken</a>  
                 </div>
-                <div class="categorie_content_c">
-                    <a class="a_btn_1" href="">Toetsenborden</a>
-                    <a class="a_btn" href="">Koptelefoons</a>
-                    <a class="a_btn" href="">Telefoons</a>
-                    <a class="a_btn_4" href="">Muizen</a>
-                </div>   
             </li>
             <li><input type="text" id="searchbar" name="searchbar" placeholder="Zoeken"></li>
             <li class="categorie_dropdown_p">
@@ -42,7 +35,7 @@
                     <a class="a_btn_1" href="">Klanten bekijken</a>
                     <a class="a_btn" href="">Klanten toevoegen</a>
                     <a class="a_btn" href="bestellingen.php">Bestellingen bekijken</a>
-                    <a class="a_btn" href="">Categorieën toevoegen</a>
+                    <a class="a_btn" href="categorietoevoegen.php">Categorieën toevoegen</a>
                     <a class="a_btn_4" href="">Landen toevoegen</a>
                 </div>   
             </li>
