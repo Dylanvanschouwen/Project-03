eb0c22d01afd5eac96f8666a341c495acef1e896
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-04-13 23:21:06 +0200

    .

--- Webpages/Geschiedenis.php
+++ Webpages/Geschiedenis.php
@@ -1,17 +1,34 @@
-<!-- This is the Gechiedenis en doelstellingen page -->
+<!-- Dit is de geschiedenis en doelstellingen pagina -->
 <!-- Author: Dylan -->
 
-<!-- Include head.html -->
+<!-- Include head.php -->
 <?php 
-include "../Webpages/include pages/head.php";
-?>
-<!-- Include navbar.html -->
-<?php 
-include "../Webpages/include pages/navbar.php";
-?>
+    include "../Webpages/include pages/head.php";
+    ?>
+<!-- Include navbar.php -->
+    <?php 
+    include "../Webpages/include pages/navbar.php";
+    ?>
 
-<main id="H_main">
+<body>
+<!-- Main code -->
+<main id="geschiedenis-main">
+<div class="Geschiedenis-doelstellingen-title">
+    <div class="Geschiedenis-title">
+    www
+    </div>
+    <div class="doelstellingen-title">
+        wwww
+    </div>
 
-    
-
+</div>
 </main>
+
+<!-- Include footer.html -->
+
+<?php
+include "../Webpages/include pages/footer.php";
+?>
+
+</body>
+
